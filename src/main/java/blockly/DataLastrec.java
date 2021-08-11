1fc6beb3f52b901e4ef67a60a5b22cda1ba42ee1
Ezequias Bento De Freitas <ezequias.freitas@memory.com.br>
2021-08-11 01:46:59 +0000

    ALTERAÇÃO MUNICIPIO - VINCULADO DE BLOCO DE NOTAS OBTER USUARIO LOGADO ALTERAÇÃO UF - VINCULO BLOCO DE NOTAS OBTER USUARIO LOGADO BLOCO DE NOTAS OBTER USUARIO LOGADO

---
+++ src/main/java/blockly/DataLastrec.java
@@ -0,0 +1,30 @@
+package blockly;
+
+import cronapi.*;
+import cronapi.rest.security.CronappSecurity;
+import java.util.concurrent.Callable;
+
+
+
+@CronapiMetaData(type = "blockly")
+@CronappSecurity
+public class DataLastrec {
+
+public static final int TIMEOUT = 300;
+
+/**
+ *
+ * @return Var
+ */
+// DataLastrec
+public static Var DataLastrec() throws Exception {
+ return new Callable<Var>() {
+
+   public Var call() throws Exception {
+    return Var.VAR_NULL;
+   }
+ }.call();
+}
+
+}
+
---
+++ src/main/java/blockly/Usuario.java
@@ -0,0 +1,61 @@
+package blockly;
+
+import cronapi.*;
+import cronapi.rest.security.CronappSecurity;
+import java.util.concurrent.Callable;
+
+
+
+@CronapiMetaData(type = "blockly")
+@CronappSecurity
+public class Usuario {
+
+public static final int TIMEOUT = 300;
+
+/**
+ *
+ * @return Var
+ */
+// Usuario
+public static Var ObterIdUsuarioLogado() throws Exception {
+ return new Callable<Var>() {
+
+   public Var call() throws Exception {
+
+    System.out.println(
+    Var.valueOf(
+    Var.valueOf("\"obtem usuname = \"").toString() +
+    cronapi.database.Operations.getField(
+    cronapi.database.Operations.query(Var.valueOf("app.entity.User"),Var.valueOf("select u from User u where u.normalizedUserName = :normalizedUserName"),Var.valueOf("normalizedUserName",
+    cronapi.util.Operations.getCurrentUserName())),
+    Var.valueOf("this[0].normalizedUserName")).toString() +
+    Var.valueOf("this[0].normalizedUserName").toString()).getObjectAsString());
+    return
+cronapi.database.Operations.getField(
+cronapi.database.Operations.query(Var.valueOf("app.entity.User"),Var.valueOf("select u from User u where u.normalizedUserName = :normalizedUserName"),Var.valueOf("normalizedUserName",
+cronapi.util.Operations.getCurrentUserName())),
+Var.valueOf("this[0].normalizedUserName"));
+   }
+ }.call();
+}
+
+/**
+ *
+ * @return Var
+ */
+// Usuario
+public static Var ObterMunicipioUsuarioLogado() throws Exception {
+ return new Callable<Var>() {
+
+   public Var call() throws Exception {
+    return
+cronapi.database.Operations.getField(
+cronapi.database.Operations.query(Var.valueOf("app.entity.User"),Var.valueOf("select u from User u where u.normalizedUserName = :normalizedUserName"),Var.valueOf("normalizedUserName",
+cronapi.util.Operations.getCurrentUserName())),
+Var.VAR_NULL);
+   }
+ }.call();
+}
+
+}
+
