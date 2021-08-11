8f8b38b4c399135d3ba3190c1932210e15ede9b5
Otaviano Francisco De Jesus Junior <otaviano.junior@memory.com.br>
2021-08-11 17:50:57 +0000

    commt

--- src/main/java/app/entity/Application.java
+++ src/main/java/app/entity/Application.java
@@ -1,11 +1,16 @@
 package app.entity;
 
-import java.io.*;
-import javax.persistence.*;
-import java.util.*;
-import javax.xml.bind.annotation.*;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import com.fasterxml.jackson.annotation.JsonFilter;
+
 import cronapi.rest.security.CronappSecurity;
 
 
