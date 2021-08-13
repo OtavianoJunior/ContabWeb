4da4323e79ff7f76626dcdf297d04a3b51bca047
Ezequias Bento De Freitas <ezequias.freitas@memory.com.br>
2021-08-13 20:05:27 +0000

    Criação de Formulário para Função / SubFunção / Programas Alteração em Home para Vinculo dos novos formulários

--- src/main/mobileapp/www/js/dataSourceMap.js
+++ src/main/mobileapp/www/js/dataSourceMap.js
@@ -1,8 +1,52 @@
 window.dataSourceMap = window.dataSourceMap || [];
-
-<?xml version="1.0" ?><error xmlns="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"><code></code><message xml:lang="en">
-(1. The content of elements must consist of well-formed character data or markup.)</message></error>
-Exception Description: An exception was thrown while processing persistence.xml from URL: file:/home/cronapp/project/target/classes/
-Exception Description: An exception was thrown while trying to load persistence unit at url: file:/home/cronapp/project/target/classes/
-Internal Exception: 
-Internal Exception: Exception [EclipseLink-30004] (Eclipse Persistence Services - 2.7.4.v20190115-ad5b7c6b2a): org.eclipse.persistence.exceptions.PersistenceUnitLoadingException
+window.dataSourceMap["app.ANOFISCALENTIDADE"] = { customId: "app.entity.ANOFISCALENTIDADE", serviceUrl: "api/cronapi/crud/app.entity.ANOFISCALENTIDADE", serviceUrlODATA: "/api/cronapi/odata/v2/app/ANOFISCALENTIDADE" };
+window.dataSourceMap["app.Application"] = { customId: "app.entity.Application", serviceUrl: "api/cronapi/crud/app.entity.Application", serviceUrlODATA: "/api/cronapi/odata/v2/app/Application" };
+window.dataSourceMap["app.ApplicationUser"] = { customId: "app.entity.ApplicationUser", serviceUrl: "api/cronapi/crud/app.entity.ApplicationUser", serviceUrlODATA: "/api/cronapi/odata/v2/app/ApplicationUser" };
+window.dataSourceMap["app.AuditLog"] = { customId: "app.entity.AuditLog", serviceUrl: "api/cronapi/crud/app.entity.AuditLog", serviceUrlODATA: "/api/cronapi/odata/v2/app/AuditLog" };
+window.dataSourceMap["app.DESPESACLASSIFICACAO"] = { customId: "app.entity.DESPESACLASSIFICACAO", serviceUrl: "api/cronapi/crud/app.entity.DESPESACLASSIFICACAO", serviceUrlODATA: "/api/cronapi/odata/v2/app/DESPESACLASSIFICACAO" };
+window.dataSourceMap["app.DESPESAORCAMENTO"] = { customId: "app.entity.DESPESAORCAMENTO", serviceUrl: "api/cronapi/crud/app.entity.DESPESAORCAMENTO", serviceUrlODATA: "/api/cronapi/odata/v2/app/DESPESAORCAMENTO" };
+window.dataSourceMap["app.Device"] = { customId: "app.entity.Device", serviceUrl: "api/cronapi/crud/app.entity.Device", serviceUrlODATA: "/api/cronapi/odata/v2/app/Device" };
+window.dataSourceMap["app.ENTIDADE"] = { customId: "app.entity.ENTIDADE", serviceUrl: "api/cronapi/crud/app.entity.ENTIDADE", serviceUrlODATA: "/api/cronapi/odata/v2/app/ENTIDADE" };
+window.dataSourceMap["app.FONTERECURSO"] = { customId: "app.entity.FONTERECURSO", serviceUrl: "api/cronapi/crud/app.entity.FONTERECURSO", serviceUrlODATA: "/api/cronapi/odata/v2/app/FONTERECURSO" };
+window.dataSourceMap["app.Login"] = { customId: "app.entity.Login", serviceUrl: "api/cronapi/crud/app.entity.Login", serviceUrlODATA: "/api/cronapi/odata/v2/app/Login" };
+window.dataSourceMap["app.MUNICIPIO"] = { customId: "app.entity.MUNICIPIO", serviceUrl: "api/cronapi/crud/app.entity.MUNICIPIO", serviceUrlODATA: "/api/cronapi/odata/v2/app/MUNICIPIO" };
+window.dataSourceMap["app.ORGAOSICOM"] = { customId: "app.entity.ORGAOSICOM", serviceUrl: "api/cronapi/crud/app.entity.ORGAOSICOM", serviceUrlODATA: "/api/cronapi/odata/v2/app/ORGAOSICOM" };
+window.dataSourceMap["app.PCA_ESTRUTURAPLANO"] = { customId: "app.entity.PCA_ESTRUTURAPLANO", serviceUrl: "api/cronapi/crud/app.entity.PCA_ESTRUTURAPLANO", serviceUrlODATA: "/api/cronapi/odata/v2/app/PCA_ESTRUTURAPLANO" };
+window.dataSourceMap["app.PPA_ACAO"] = { customId: "app.entity.PPA_ACAO", serviceUrl: "api/cronapi/crud/app.entity.PPA_ACAO", serviceUrlODATA: "/api/cronapi/odata/v2/app/PPA_ACAO" };
+window.dataSourceMap["app.PPA_FUNCAO"] = { customId: "app.entity.PPA_FUNCAO", serviceUrl: "api/cronapi/crud/app.entity.PPA_FUNCAO", serviceUrlODATA: "/api/cronapi/odata/v2/app/PPA_FUNCAO" };
+window.dataSourceMap["app.PPA_PROGRAMA"] = { customId: "app.entity.PPA_PROGRAMA", serviceUrl: "api/cronapi/crud/app.entity.PPA_PROGRAMA", serviceUrlODATA: "/api/cronapi/odata/v2/app/PPA_PROGRAMA" };
+window.dataSourceMap["app.PPA_SUBFUNCAO"] = { customId: "app.entity.PPA_SUBFUNCAO", serviceUrl: "api/cronapi/crud/app.entity.PPA_SUBFUNCAO", serviceUrlODATA: "/api/cronapi/odata/v2/app/PPA_SUBFUNCAO" };
+window.dataSourceMap["app.PPA_UNIDADEORCAMENTO"] = { customId: "app.entity.PPA_UNIDADEORCAMENTO", serviceUrl: "api/cronapi/crud/app.entity.PPA_UNIDADEORCAMENTO", serviceUrlODATA: "/api/cronapi/odata/v2/app/PPA_UNIDADEORCAMENTO" };
+window.dataSourceMap["app.Role"] = { customId: "app.entity.Role", serviceUrl: "api/cronapi/crud/app.entity.Role", serviceUrlODATA: "/api/cronapi/odata/v2/app/Role" };
+window.dataSourceMap["app.RoleSecurable"] = { customId: "app.entity.RoleSecurable", serviceUrl: "api/cronapi/crud/app.entity.RoleSecurable", serviceUrlODATA: "/api/cronapi/odata/v2/app/RoleSecurable" };
+window.dataSourceMap["app.Securable"] = { customId: "app.entity.Securable", serviceUrl: "api/cronapi/crud/app.entity.Securable", serviceUrlODATA: "/api/cronapi/odata/v2/app/Securable" };
+window.dataSourceMap["app.UF"] = { customId: "app.entity.UF", serviceUrl: "api/cronapi/crud/app.entity.UF", serviceUrlODATA: "/api/cronapi/odata/v2/app/UF" };
+window.dataSourceMap["app.USERACESSO"] = { customId: "app.entity.USERACESSO", serviceUrl: "api/cronapi/crud/app.entity.USERACESSO", serviceUrlODATA: "/api/cronapi/odata/v2/app/USERACESSO" };
+window.dataSourceMap["app.User"] = { customId: "app.entity.User", serviceUrl: "api/cronapi/crud/app.entity.User", serviceUrlODATA: "/api/cronapi/odata/v2/app/User" };
+window.dataSourceMap["app.UserRole"] = { customId: "app.entity.UserRole", serviceUrl: "api/cronapi/crud/app.entity.UserRole", serviceUrlODATA: "/api/cronapi/odata/v2/app/UserRole" };
+window.dataSourceMap["app.UserSecurable"] = { customId: "app.entity.UserSecurable", serviceUrl: "api/cronapi/crud/app.entity.UserSecurable", serviceUrlODATA: "/api/cronapi/odata/v2/app/UserSecurable" };
+window.dataSourceMap["app.View"] = { customId: "app.entity.View", serviceUrl: "api/cronapi/crud/app.entity.View", serviceUrlODATA: "/api/cronapi/odata/v2/app/View" };
+window.dataSourceMap["app.auditlogquery"] = { customId: "auditlogquery", serviceUrl: "api/cronapi/odata/v2/app/auditlogquery/", serviceUrlODATA: "/api/cronapi/odata/v2/app/auditlogquery" };
+window.dataSourceMap["app.auth"] = { customId: "auth", serviceUrl: "api/cronapi/odata/v2/app/auth/", serviceUrlODATA: "/api/cronapi/odata/v2/app/auth" };
+window.dataSourceMap["app.device"] = { customId: "device", serviceUrl: "api/cronapi/query/1jy88nt41afza/", serviceUrlODATA: "/api/cronapi/odata/v2/app/device" };
+window.dataSourceMap["app.query105225"] = { customId: "query105225", serviceUrl: "api/cronapi/odata/v2/app/query105225/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query105225" };
+window.dataSourceMap["app.query118609"] = { customId: "query118609", serviceUrl: "api/cronapi/odata/v2/app/query118609/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query118609" };
+window.dataSourceMap["app.query167888"] = { customId: "query167888", serviceUrl: "api/cronapi/odata/v2/app/query167888/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query167888" };
+window.dataSourceMap["app.query438318"] = { customId: "query438318", serviceUrl: "api/cronapi/odata/v2/app/query438318/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query438318" };
+window.dataSourceMap["app.query489074"] = { customId: "query489074", serviceUrl: "api/cronapi/odata/v2/app/query489074/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query489074" };
+window.dataSourceMap["app.query669256"] = { customId: "query669256", serviceUrl: "api/cronapi/odata/v2/app/query669256/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query669256" };
+window.dataSourceMap["app.query726367"] = { customId: "query726367", serviceUrl: "api/cronapi/odata/v2/app/query726367/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query726367" };
+window.dataSourceMap["app.query78386"] = { customId: "query78386", serviceUrl: "api/cronapi/odata/v2/app/query78386/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query78386" };
+window.dataSourceMap["app.query892456"] = { customId: "query892456", serviceUrl: "api/cronapi/odata/v2/app/query892456/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query892456" };
+window.dataSourceMap["app.query951406"] = { customId: "query951406", serviceUrl: "api/cronapi/odata/v2/app/query951406/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query951406" };
+window.dataSourceMap["app.roleManager"] = { customId: "roleManager", serviceUrl: "api/cronapi/odata/v2/app/roleManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/roleManager" };
+window.dataSourceMap["app.roleSecurableManager"] = { customId: "roleSecurableManager", serviceUrl: "api/cronapi/odata/v2/app/roleSecurableManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/roleSecurableManager" };
+window.dataSourceMap["app.roleSecurables"] = { customId: "roleSecurables", serviceUrl: "api/cronapi/odata/v2/app/roleSecurables/", serviceUrlODATA: "/api/cronapi/odata/v2/app/roleSecurables" };
+window.dataSourceMap["app.roles"] = { customId: "roles", serviceUrl: "api/cronapi/odata/v2/app/roles/", serviceUrlODATA: "/api/cronapi/odata/v2/app/roles" };
+window.dataSourceMap["app.securableManager"] = { customId: "securableManager", serviceUrl: "api/cronapi/odata/v2/app/securableManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/securableManager" };
+window.dataSourceMap["app.securableViews"] = { customId: "securableViews", serviceUrl: "api/cronapi/odata/v2/app/securableViews/", serviceUrlODATA: "/api/cronapi/odata/v2/app/securableViews" };
+window.dataSourceMap["app.securables"] = { customId: "securables", serviceUrl: "api/cronapi/odata/v2/app/securables/", serviceUrlODATA: "/api/cronapi/odata/v2/app/securables" };
+window.dataSourceMap["app.userManager"] = { customId: "userManager", serviceUrl: "api/cronapi/odata/v2/app/userManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/userManager" };
+window.dataSourceMap["app.userRoleManager"] = { customId: "userRoleManager", serviceUrl: "api/cronapi/odata/v2/app/userRoleManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/userRoleManager" };
+window.dataSourceMap["app.userSecurableManager"] = { customId: "userSecurableManager", serviceUrl: "api/cronapi/odata/v2/app/userSecurableManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/userSecurableManager" };
+window.dataSourceMap["app.userSecurables"] = { customId: "userSecurables", serviceUrl: "api/cronapi/odata/v2/app/userSecurables/", serviceUrlODATA: "/api/cronapi/odata/v2/app/userSecurables" };
