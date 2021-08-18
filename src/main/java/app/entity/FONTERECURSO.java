8f3a7f4371a6e86370a1f47bb7c579426da9c282
Ezequias Bento De Freitas <ezequias.freitas@memory.com.br>
2021-08-18 16:37:05 +0000

    Alteração feitos em testes - UO / UF / Orgão/ Função / Subfunção e Progrmas. Inlcusão Fonte de recurso

--- src/main/java/app/entity/FONTERECURSO.java
+++ src/main/java/app/entity/FONTERECURSO.java
@@ -51,13 +51,6 @@ public class FONTERECURSO implements Serializable {
     /**
     * @generated
     */
-    @Column(name = "COD_FONTERECURSODESTINACAOSUP", nullable = true, unique = false, length=20, insertable=true, updatable=true)
-        
-        private java.lang.String coD_FONTERECURSODESTINACAOSUP;
-
-    /**
-    * @generated
-    */
     @Column(name = "COD_FONTESEMMASCARA", nullable = true, unique = false, length=20, insertable=true, updatable=true)
         
         private java.lang.String coD_FONTESEMMASCARA;
@@ -105,6 +98,14 @@ public class FONTERECURSO implements Serializable {
     @JoinColumn(name="ID_ANOFISCAL", nullable = true, referencedColumnName = "ID_ANOFISCAL", insertable=true, updatable=true, foreignKey = @ForeignKey(name = ""))
         
         private ANOFISCALENTIDADE id_ANOFISCAL;
+
+    /**
+    * @generated
+    */
+    @ManyToOne
+    @JoinColumn(name="fk_FONTERECURSO_0", nullable = true, referencedColumnName = "ID_FONTERECURSO", insertable=true, updatable=true, foreignKey = @ForeignKey(name = ""))
+        
+        private FONTERECURSO id_FONTERECURSOSUP;
 
     /**
     * Construtor
@@ -168,25 +169,6 @@ public class FONTERECURSO implements Serializable {
     */
     public FONTERECURSO setDeS_FONTERECURSO(java.lang.String deS_FONTERECURSO){
         this.deS_FONTERECURSO = deS_FONTERECURSO;
-        return this;
-    }
-    /**
-    * Obtém coD_FONTERECURSODESTINACAOSUP
-    * return coD_FONTERECURSODESTINACAOSUP
-    * @generated
-    */
-    
-    public java.lang.String getCoD_FONTERECURSODESTINACAOSUP(){
-        return this.coD_FONTERECURSODESTINACAOSUP;
-    }
-
-    /**
-    * Define coD_FONTERECURSODESTINACAOSUP
-    * @param coD_FONTERECURSODESTINACAOSUP coD_FONTERECURSODESTINACAOSUP
-    * @generated
-    */
-    public FONTERECURSO setCoD_FONTERECURSODESTINACAOSUP(java.lang.String coD_FONTERECURSODESTINACAOSUP){
-        this.coD_FONTERECURSODESTINACAOSUP = coD_FONTERECURSODESTINACAOSUP;
         return this;
     }
     /**
@@ -320,6 +302,25 @@ public class FONTERECURSO implements Serializable {
     */
     public FONTERECURSO setId_ANOFISCAL(ANOFISCALENTIDADE id_ANOFISCAL){
         this.id_ANOFISCAL = id_ANOFISCAL;
+        return this;
+    }
+    /**
+    * Obtém id_FONTERECURSOSUP
+    * return id_FONTERECURSOSUP
+    * @generated
+    */
+    
+    public FONTERECURSO getId_FONTERECURSOSUP(){
+        return this.id_FONTERECURSOSUP;
+    }
+
+    /**
+    * Define id_FONTERECURSOSUP
+    * @param id_FONTERECURSOSUP id_FONTERECURSOSUP
+    * @generated
+    */
+    public FONTERECURSO setId_FONTERECURSOSUP(FONTERECURSO id_FONTERECURSOSUP){
+        this.id_FONTERECURSOSUP = id_FONTERECURSOSUP;
         return this;
     }
 
