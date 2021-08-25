53342dd6fbe6b43d0213b64fe634055de003bf80
Renata Ettore Do Valle <renata.valle@memory.com.br>
2021-08-25 20:41:39 +0000

    Alterações Formulários de Adiantamentos

--- src/main/java/blockly/TipoAdiantamento.java
+++ src/main/java/blockly/TipoAdiantamento.java
@@ -48,10 +48,45 @@ Var.valueOf("PCA_PLANOCONTAS.active.deS_CONTA"));
 public static Var ExcluiSubtipos() throws Exception {
  return new Callable<Var>() {
 
+   private Var item = Var.VAR_NULL;
+   private Var subtipo = Var.VAR_NULL;
+   private Var sql = Var.VAR_NULL;
+
    public Var call() throws Exception {
 
-    cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.removeRecord"),
-    Var.valueOf("PCA_SUBTIPOADIANTAMENTO"));
+    item =
+    cronapi.screen.Operations.getValueOfField(
+    Var.valueOf("PCA_TIPOADIANTAMENTO.active.id_TIPOADIANTAMENTO"));
+
+    subtipo =
+    cronapi.screen.Operations.getValueOfField(
+    Var.valueOf("PCA_SUBTIPOADIANTAMENTO.active.id_TIPOADIANTAMENTO"));
+
+    System.out.println(
+    Var.valueOf(
+    Var.valueOf("id tipo = ").toString() +
+    item.toString() +
+    Var.valueOf("id subtipo = ").toString() +
+    subtipo.toString()).getObjectAsString());
+
+    System.out.println(
+    Var.valueOf(
+    Var.valueOf("ID_TIPOADIANTAMENTO").toString() +
+    Var.valueOf("=").toString() +
+    cronapi.screen.Operations.getValueOfField(
+    Var.valueOf("PCA_TIPOADIANTAMENTO.active.id_TIPOADIANTAMENTO")).toString()).getObjectAsString());
+
+    sql =
+    Var.valueOf(
+    Var.valueOf("id_TIPOADIANTAMENTO").toString() +
+    Var.valueOf(" eq \'").toString() +
+    cronapi.screen.Operations.getValueOfField(
+    Var.valueOf("PCA_TIPOADIANTAMENTO.active.id_TIPOADIANTAMENTO")).toString() +
+    Var.valueOf("\'").toString());
+
+    cronapi.database.Operations.execute(Var.valueOf("app.entity.PCA_SUBTIPOADIANTAMENTO"), Var.valueOf("delete from PCA_SUBTIPOADIANTAMENTO where id_TIPOADIANTAMENTO = :tipoadiantamento"),Var.valueOf("tipoadiantamento",
+    cronapi.screen.Operations.getValueOfField(
+    Var.valueOf("PCA_TIPOADIANTAMENTO.active.id_TIPOADIANTAMENTO"))));
     return Var.VAR_NULL;
    }
  }.call();
