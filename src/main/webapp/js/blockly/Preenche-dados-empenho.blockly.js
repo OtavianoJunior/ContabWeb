275c36098875bb508e298bff50aabda9d62fe158
Daniel Augusto De Castro Franco <daniel.franco@memory.com.br>
2021-09-08 17:00:06 +0000

    Commit - Empenho att

---
+++ src/main/webapp/js/blockly/Preenche-dados-empenho.blockly.js
@@ -0,0 +1,12 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.PreencheDadosEmpenho = window.blockly.js.blockly.PreencheDadosEmpenho || {};
+
+/**
+ * Preenche-dados-empenho
+ */
+window.blockly.js.blockly.PreencheDadosEmpenho.preencher_dadosArgs = [];
+window.blockly.js.blockly.PreencheDadosEmpenho.preencher_dados = async function() {
+
+}
